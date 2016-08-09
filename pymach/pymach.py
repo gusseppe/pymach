7b5d3ce994863ba5f8fe38d329dddd896aaa52b2
Gusseppe Bravo <hackersvlc@gmail.com>
2016-08-09 17:09:22 -0500

    python 3

--- pymach/pymach.py
+++ pymach/pymach.py
@@ -5,10 +5,10 @@ from prepare import Prepare
 obj = Analyze(className='species')
 obj.read("iris.csv")
 #print obj.data
-print obj.description()
+#print obj.description()
 #print obj.classBalance()
 #print obj.hist()
 #print obj.density()
 #print obj.corr()
-print obj.scatter()
+print(obj.scatter())
 
