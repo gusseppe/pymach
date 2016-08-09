aceef1c689aa7fd7db5fa103e490971e375029bd
Gusseppe Bravo <hackersvlc@gmail.com>
2016-08-09 17:43:05 -0500

    python 3

--- pymach/pymach.py
+++ pymach/pymach.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-#from analyze import Analyze
-#from prepare import Prepare
+from analyze import Analyze
+from prepare import Prepare
 
-#obj = Analyze(className='species')
-#obj.read("iris.csv")
+obj = Analyze(className='species')
+obj.read("iris.csv")
 #print obj.data
 #print obj.description()
 #print obj.classBalance()
 #print obj.hist()
 #print obj.density()
 #print obj.corr()
-#print(obj.scatter())
+print(obj.scatter())
 
