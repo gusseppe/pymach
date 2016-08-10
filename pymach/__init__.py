944a0038f0db714dd59dc7d62a0ba6980624f045
Gusseppe Bravo <hackersvlc@gmail.com>
2016-08-09 19:08:47 -0500

    python 3

--- pymach/__init__.py
+++ pymach/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-__all__ = ['analyze', 'prepare']
 
+from analyze import Analyze
+
+__all__ = ['analyze', 'prepare']
 __author__ = 'Gusseppe Bravo Rocca'
 __email__ = 'gbravor@uni.pe'
 __version__ = '0.1.0'
