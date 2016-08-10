995c02e2407a40d5e40f417fdc37a72cc102e448
Gusseppe Bravo <hackersvlc@gmail.com>
2016-08-09 20:14:30 -0500

    python 3

--- pymach/__init__.py
+++ pymach/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from analyze import Analyze
-
 __all__ = ['analyze', 'prepare']
 __author__ = 'Gusseppe Bravo Rocca'
 __email__ = 'gbravor@uni.pe'
--- pymach/pymach.py
+++ pymach/pymach.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 #import analyze
-from analyze import Analyze
-
+import analyze
 #print obj.data
 #print obj.description()
 #print obj.classBalance()
 #print obj.hist()
 #print obj.density()
 #print obj.corr()
-if __name__ == "__main__":
-    obj = Analyze(className='species')
+#if __name__ == "__main__":
+obj = analyze.Analyze(className='species')
     #obj.read("iris.csv")
     #print(obj.scatter())
 
