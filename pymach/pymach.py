f444db572bc0c4d5880f8070e18360ad68dbd41b
Gusseppe Bravo <hackersvlc@gmail.com>
2016-09-27 19:36:53 -0500

    Define module

--- pymach/pymach.py
+++ pymach/pymach.py
@@ -8,27 +8,16 @@
 This module provides the logic of the whole project.
 
 """
+#import define
 #import analyze
-import define
-import analyze
-import prepare
-#print obj.data
-#print obj.description()
-#print obj.classBalance()
-#print obj.hist()
-#print obj.density()
-#print obj.corr()
-if __name__ == "__main__":
-    analyzer = analyze.Analyze(define)
-    d = analyzer.read()
-    preparer = prepare.Prepare(define)
-    preparer = preparer.pipeline()
-    print preparer
-    y = preparer.fit_transform(d)
-    print y[0:2,:]
-    #print d
-    #analyzer.pipeline()
-    #print analyzer.data
-    #obj.read("iris.csv")
-    #print(obj.scatter())
+#import prepare
+
+#if __name__ == "__main__":
+    #analyzer = analyze.Analyze(define)
+    #d = analyzer.read()
+    #preparer = prepare.Prepare(define)
+    #preparer = preparer.pipeline()
+    #print preparer
+    #y = preparer.fit_transform(d)
+    #print y[0:2,:]
 
