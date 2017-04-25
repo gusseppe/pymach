6518e2dc0997664e46084147619af5a7dfc97ae4
Gusseppe Bravo <hackersvlc@gmail.com>
2017-04-25 04:42:59 -0500

    Model plot

--- pymach/pymach.py
+++ pymach/pymach.py
@@ -8,48 +8,48 @@
 This module provides the logic of the whole project.
 
 """
-import define
-import analyze
-import prepare
-import feature_selection
-import evaluate
+#import define
+#import analyze
+#import prepare
+#import feature_selection
+#import evaluate
 
-import time
+#import time
 
-name = "datasets/iris.csv"
-#name = "datasets/miningData.csv"
-#name = "datasets/LocalizationOld.csv"
-#name = "datasets/seguridad.csv"
-#name = "datasets/breast-cancer-wisconsin.csv"
-#name = "breast-cancer-wisconsin.csv"
-#name = "inputBus.csv"
-# className = "Ruta"
-#className = "CATEGORY"
-#className = "accident_type"
-#className = "position"
-className = "class"
+#name = "datasets/iris.csv"
+##name = "datasets/miningData.csv"
+##name = "datasets/LocalizationOld.csv"
+##name = "datasets/seguridad.csv"
+##name = "datasets/breast-cancer-wisconsin.csv"
+##name = "breast-cancer-wisconsin.csv"
+##name = "inputBus.csv"
+## className = "Ruta"
+##className = "CATEGORY"
+##className = "accident_type"
+##className = "position"
+#className = "class"
 
-def main():
-    #STEP 0: Define workflow parameters
-    definer = define.Define(nameData=name, className=className).pipeline()
+#def main():
+    ##STEP 0: Define workflow parameters
+    #definer = define.Define(nameData=name, className=className).pipeline()
 
-    #STEP 1: Analyze data by ploting it
-    #analyze.Analyze(definer).pipeline()
+    ##STEP 1: Analyze data by ploting it
+    ##analyze.Analyze(definer).pipeline()
 
-    #STEP 2: Prepare data by scaling, normalizing, etc. 
-    preparer = prepare.Prepare(definer).pipeline()
+    ##STEP 2: Prepare data by scaling, normalizing, etc. 
+    #preparer = prepare.Prepare(definer).pipeline()
 
-    #STEP 3: Feature selection
-    featurer = feature_selection.FeatureSelection(definer).pipeline()
+    ##STEP 3: Feature selection
+    #featurer = feature_selection.FeatureSelection(definer).pipeline()
 
-    #STEP4: Evalute the algorithms by using the pipelines
-    #evaluator = evaluate.Evaluate(definer, preparer, featurer).pipeline()
+    ##STEP4: Evalute the algorithms by using the pipelines
+    ##evaluator = evaluate.Evaluate(definer, preparer, featurer).pipeline()
 
-if __name__ == '__main__':
-    start = time.time()
-    main()
-    end = time.time()
+#if __name__ == '__main__':
+    #start = time.time()
+    #main()
+    #end = time.time()
 
-    print()
-    print("Execution time for all the steps: ", end-start)
+    #print()
+    #print("Execution time for all the steps: ", end-start)
 
