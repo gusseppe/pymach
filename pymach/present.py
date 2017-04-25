1c54e22737a0467f651164c984748032bfb1aec5
Gusseppe Bravo <hackersvlc@gmail.com>
2017-04-25 04:57:13 -0500

    Model plot

--- pymach/present.py
+++ pymach/present.py
@@ -1,113 +1,144 @@
+#Standard Libraries
 import os
+import datetime
+import json
+import random
+
+#Third Libraries
+import pandas as pd
+import numpy as np
+
+#Local Libraries
 import define
 import analyze
 import prepare
 import feature_selection
 import evaluate
-import json
-import random
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
-import numpy as np
-import datetime
-import mpld3
 
-from flask import Flask, flash, render_template, redirect, json, request, url_for, jsonify
+from flask import Flask, flash, render_template, \
+        redirect, json, request, url_for, jsonify
 from threading import Lock
 from mpld3 import plugins
 from werkzeug import secure_filename
 from datetime import datetime
-
-matplotlib.use('Agg')
-plt.ioff()
-
-lock = Lock()
+from collections import OrderedDict
 
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['MODELS_FOLDER'] = 'models'
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
+app.config['UPLOAD_FOLDER'] = os.path.join(APP_PATH, 'uploads')
+app.config['MODELS_FOLDER'] = os.path.join(APP_PATH, 'models')
 ALLOWED_EXTENSIONS = ['txt', 'csv', 'ml']
 
-# Setting up matplotlib sytles using BMH
-s = json.load(open("./static/bmh_matplotlibrc.json"))
-matplotlib.rcParams.update(s)
 
 #name = "iris.csv"
 header = None
 
-def draw_fig(fig_type, className, name):
+def draw_fig(figures, className, name):
 
-    #with lock:
-    #fig, ax = plt.subplots()
-    fig, ax = plt.subplots(figsize=(10, 7))
-    definer = define.Define(name, header, className).pipeline()
+    definer = define.Define(
+            data_name=name,
+            header=header,
+            class_name=className).pipeline()
+
     analyzer = analyze.Analyze(definer)
-    #fig = plt.figure(figsize=(10,10))
-    if fig_type == "data":
-        d = definer.data
-        return d.head(20).to_html(show_dimensions=True)
-    elif fig_type == "description":
-        desc = analyzer.description()
-        return desc.to_html(show_dimensions=True)
-    elif fig_type == "hist":
-        analyzer.hist(ax)
-    elif fig_type == "box":
-        analyzer.box(ax)
-    elif fig_type == "density":
-        analyzer.density(ax)
-    elif fig_type == "corr":
-        analyzer.corr(ax)
-    elif fig_type == "scatter":
-        analyzer.scatter(ax)
-    elif fig_type == "model":
-        preparer = prepare.Prepare(definer).pipeline()
-        featurer = feature_selection.FeatureSelection(definer).pipeline()
-        evaluator = evaluate.Evaluate(definer, preparer, featurer).pipeline()
-        results = evaluator.report
-        return results.to_html(show_dimensions=True)
+
+    dict_figures = OrderedDict()
+    for fig in figures:
+        dict_figures[fig] = analyzer.plot(fig)
+        #if fig == "description":
+            #desc = analyzer.description()
+            #return desc.to_html(show_dimensions=True)
+        #elif fig == "model":
+            #preparer = prepare.Prepare(definer).pipeline()
+            #featurer = feature_selection.FeatureSelection(definer).pipeline()
+            #evaluator = evaluate.Evaluate(definer, preparer, featurer).pipeline()
+            #results = evaluator.report
+            #return results.to_html(show_dimensions=True)
     
-    return mpld3.fig_to_html(fig)
+    return dict_figures
+
+def report_model(class_name, data_name):
+    definer = define.Define(
+            data_name=data_name,
+            header=header,
+            class_name=class_name).pipeline()
+
+    preparer = prepare.Prepare(definer).pipeline()
+    featurer = feature_selection.FeatureSelection(definer).pipeline()
+    evaluator = evaluate.Evaluate(definer, preparer, featurer).pipeline()
+    #results = evaluator.report
+
+    #print(evaluator.plot_models()) 
+    plot = evaluator.plot_models()
+    table = evaluator.report
+    dict_report = {'plot':plot, 'table':table}
+    #return  evaluator.plot_models()
+        
+    #return results.to_html(show_dimensions=True)
+
+    return dict_report
+    #return mpld3.fig_to_html(fig)
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+#@app.route('/')
+#def home():
+    #return render_template("uploadData.html")
+
+########################### Start Upload Button ##################################
 @app.route('/')
-def home():
-    return render_template("home.html")
-
 @app.route('/defineData', methods = ['GET', 'POST'])
 def defineData():
+    """  Show the files which have been uploaded """
     dirs = os.listdir(app.config['UPLOAD_FOLDER'])
-    #print(dirs)
     return render_template('uploadData.html', files = dirs)	
 
 @app.route('/storeData', methods = [ 'GET', 'POST'])
-def guardarData():
+def storedata():
+    """  Upload a new file """
     if request.method == 'POST':
-            file = request.files['file']
-            if file and allowed_file(file.filename):
-                    now = datetime.now()
-                    filename = os.path.join(app.config['UPLOAD_FOLDER'], "%s" % (file.filename))
-                    file.save(filename)
-                    return jsonify({"success":True})
-            return redirect(url_for('defineData'))
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            now = datetime.now()
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], "%s" % (file.filename))
+            file.save(filename)
+            return jsonify({"success":True})
+
+        return redirect(url_for('defineData'))
     else:
-            return redirect(url_for('home'))
+        return redirect(url_for('defineData'))
 
 @app.route('/chooseData', methods = ['GET', 'POST'])
 def chooseData():
-    #data = json.loads(request.data)
+    """  choose a file and show its content """
+    from itertools import islice
     plot_type = 'data'
     classname = "class"
-    name = 'uploads/'
+    data_name = ''
+    path_name = ''
     dirs = os.listdir(app.config['UPLOAD_FOLDER'])
     if request.method == 'POST':
-        name = name + request.form['submit']
-    return render_template('uploadData.html', files = dirs, dataframe=draw_fig(plot_type, classname, name))	
+        data_name = request.form['submit']
+        path_data = os.path.join(app.config['UPLOAD_FOLDER'], data_name)
 
+    dataset = []
+    with open(path_data) as myfile:
+        dataset = list(islice(myfile, 20))
+        dataset = [line[:-1] for line in dataset]
+
+    return render_template(
+            'uploadData.html',
+            files = dirs,
+            dataset=dataset,
+            data_name=data_name)	
+
+
+########################### End Upload Button ##################################
+
+########################### Start Analyze Button ##################################
 @app.route('/analyzeData', methods = ['GET', 'POST'])
 def analyzeData():
     dirs = os.listdir(app.config['UPLOAD_FOLDER'])
@@ -115,17 +146,24 @@ def analyzeData():
 
 @app.route('/plotData', methods = ['GET', 'POST'])
 def plotData():
-    plot_type1 = 'hist'
-    plot_type2 = 'corr'
-    plot_type3 = 'scatter'
+    figures = ['histogram', 'box', 'corr']
     classname = "class"
-    name = 'uploads/'
+    data_name = ''
+    path_name = ''
     dirs = os.listdir(app.config['UPLOAD_FOLDER'])
     if request.method == 'POST':
-        name = name + request.form['submit']
-    ##print(dirs)
-    return render_template('analyzeData.html', files = dirs, plot1=draw_fig(plot_type1, classname, name), plot2=draw_fig(plot_type2, classname, name), plot3=draw_fig(plot_type3, classname, name))	
-	
+        data_name = request.form['submit']
+        path_data = os.path.join(app.config['UPLOAD_FOLDER'], data_name)
+
+    return render_template(
+            'analyzeData.html',
+            files=dirs,
+            figures=draw_fig(figures, classname, path_data),
+            data_name=data_name)	
+
+########################### End Analyze Button ##################################
+
+########################### Start Model Button ##################################
 @app.route('/models', methods = ['GET', 'POST'])
 def models():
     dirs = os.listdir(app.config['UPLOAD_FOLDER'])
@@ -133,24 +171,22 @@ def models():
 
 @app.route('/modelingData', methods = ['GET', 'POST'])
 def modelingData():
-    plot_type = 'model'
+    #plot_type = 'model'
     classname = "class"
-    name = 'uploads/'
+    data_name = ''
+    path_name = ''
     dirs = os.listdir(app.config['UPLOAD_FOLDER'])
     if request.method == 'POST':
-        name = name + request.form['submit']
-    return render_template('models.html', files=dirs, model=draw_fig(plot_type, classname, name))	
+        data_name = request.form['submit']
+        path_data = os.path.join(app.config['UPLOAD_FOLDER'], data_name)
+        #name = name + request.form['submit']
+    return render_template(
+            'models.html', 
+            files=dirs,
+            report=report_model(classname, path_data),
+            data_name=data_name)	
 
-#@app.route('/prediction', methods = ['GET', 'POST'])
-#def prediction():
-	#dirs = os.listdir(app.config['MODELS_FOLDER'])
-        #print dirs
-	#return render_template('prediction.html', files = dirs)	
-
-#@app.route('/predictData', methods = ['GET', 'POST'])
-#def predictionData():
-	#dirs = os.listdir(app.config['MODELS_FOLDER'])
-	#return render_template('prediction.html', files = dirs)	
+########################### End Model Button ##################################
 
 @app.route('/prediction', methods = ['GET', 'POST'])
 def prediction():
