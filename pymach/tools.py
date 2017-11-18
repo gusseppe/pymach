ec98d83dfc1572fc044b3afb0ab31d967c14f07c
Gusseppe Bravo <hackersvlc@gmail.com>
2017-11-17 20:01:35 -0500

    improve

--- pymach/tools.py
+++ pymach/tools.py
@@ -73,11 +73,11 @@ def total_report():
     import evaluate
     import improve
 
-    label = 'LocalizationNew_Tx'
-    #label = 'BQ_AQUARIS_E5_'
+    # label = 'LocalizationNew_Tx'
+    label = 'BQ_AQUARIS_E5_'
 
-    for i in range(7, 8):
-    #for i in [20,30,40]:
+    # for i in range(7, 8):
+    for i in ['20','30','40']:
 
         data_name = "./uploads/"+label+str(i)+".csv"
         print(data_name)
